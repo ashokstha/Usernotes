5547f028238a2b5e95733415408ba87c98e36721
akstha007 <cs.ashok007@gmail.com>
2021-12-29 19:37:02 -0800

    init project commit

---
+++ src/main/java/com/uashuke/livefeed/usernotes/UsernotesApplication.java
@@ -0,0 +1,13 @@
+package com.uashuke.livefeed.usernotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsernotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsernotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uashuke/livefeed/usernotes/UsernotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uashuke.livefeed.usernotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsernotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
